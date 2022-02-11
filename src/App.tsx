821ab97138c652dc23f928f35e5c745622bb19b5
dandeduck <danielmkhlv@gmail.com>
2022-02-11 02:43:33 +0200

    Turned player into a context and added session loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { io, Socket } from "socket.io-client";
 import './App.css';
 import { v4 as uuid } from 'uuid';
@@ -7,33 +7,47 @@ import { Player } from './components/api/player';
 import NamePicker from './components/name-picker/NamePicker';
 import Logo from './components/logo/Logo';
 
-export const playerContext = createContext({});
+export const playerContext = createContext<Player>({id: "", name: ""});
 
 function App() {
-  const [player, setPlayer] = useState<Player>();
+  const [player, setPlayer] = useState<Player>({id: "", name: ""});
 
-  useEffect(() => {
-    const socket = io("ws://localhost:4001", {transports: ['websocket']});
-    const player = {name: "Daniel", id: uuid()};
+  // useEffect(() => {
+  //   const socket = io("ws://localhost:4001", {transports: ['websocket']});
+  //   const player = {name: "Daniel", id: uuid()};
 
-    socket.on("connect", () => {
-      initGame(player, socket);
-    });
-  });
+  //   socket.on("connect", () => {
+  //     initGame(player, socket);
+  //   });
+  // }, []);
   
-  const initGame = async (player: Player, socket: Socket) => {
-    const roomNumber = (await createRoom(player, {isOpen: true, size: 5, doodleTime: 30, length: 2, categories: ["person", "activity"]})).roomNumber;
-    await joinRoom(player, roomNumber);
-    socket.emit("join", {player: player, room: roomNumber}, (response: any) => {
-      console.log(response);
-    });
-  }
+  // const initGame = async (player: Player, socket: Socket) => {
+  //   const roomNumber = (await createRoom(player, {isOpen: true, size: 5, doodleTime: 30, length: 2, categories: ["person", "activity"]})).roomNumber;
+  //   await joinRoom(player, roomNumber);
+  //   socket.emit("join", {player: player, room: roomNumber}, (response: any) => {
+  //     console.log(response);
+  //   });
+  // }
+  useEffect(() => {
+    if (player.id === "") {
+      const stored = sessionStorage.getItem('player');
+
+      if (stored && stored.length) {
+        const parsed = JSON.parse(stored);
+
+        if (parsed.id !== "")
+          setPlayer(parsed);
+      }
+    }
+    else
+      sessionStorage.setItem('player', JSON.stringify(player));
+  }, [player]);
 
   return (
     <div className="app">
       <Logo/>
-      <playerContext.Provider value={[player, setPlayer]}>
-        <NamePicker setPlayer={setPlayer}/>
+      <playerContext.Provider value={player}>
+        {player.id == "" ? <NamePicker setPlayer={setPlayer}/> : <span></span>}
       </playerContext.Provider>
     </div>
   );
--- src/components/name-picker/NamePicker.tsx
+++ src/components/name-picker/NamePicker.tsx
@@ -1,27 +1,32 @@
-import { useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { Player } from '../api/player';
 import { v4 as uuid} from 'uuid';
+import gsap from 'gsap';
 import './NamePicker.css';
+import { playerContext } from '../../App';
 
 export default function NamePicker(props: {setPlayer: (player: Player) => void}) {
   const [playerName, setPlayerName] = useState("");
-  const ref = useRef<HTMLDivElement>(null);
   
   const onClick = () => {
-    props.setPlayer({
+    gsap.to('.name-picker .container', {
+      y: -200,
+      opacity: 0,
+      duration: 0.3
+    })
+    gsap.to('.name-picker', {
+      opacity: 0,
+      duration: 0.3
+    })
+
+    setTimeout(() => props.setPlayer({
       name: playerName,
       id: uuid()
-    });
-    
-    const element = ref.current;
-    console.log(element);
-
-    if (element)
-      element.style.visibility = 'hidden';
+    }), 600);
   }
 
   return (
-    <div className='name-picker' ref={ref}>
+    <div className='name-picker'>
       <div className='container'>
         <div className='form'>
           <input className='name hand-written' placeholder='Name' onChange={e => {setPlayerName(e.target.value)}}></input>
